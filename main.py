aca561ff813ace65864dd2bbf42f41dfea99e6a9
xxA2i <ilyasavin59@gmail.com>
2024-03-22 19:46:44 +0300

    Done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('____---Calculator---____')
+
+a = input('Enter a number: ')
+b = input('Enter b number: ')
+c = input('Enter c number: ')
+d = input('Enter d number: ')
+e = input('Enter e number: ')
+f = input('Enter f number: ')
+
+print(a, '+', b, '= 2')
+print(c, '+', d, '= 2')
+print(e, '+', f, '= 2')
